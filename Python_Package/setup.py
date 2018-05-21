702332157e79a103cb11bee418cae4642c562eb2
T M Sudarson <15136@en-rajeshv.mynt.myntra.com>
2018-05-21 14:56:07 +0530

    Python Package

---
+++ Python_Package/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'ESH2',
+  packages = ['ESH2'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Find which model and parameter set fits best ',
+  author = 'Sudarson Mothilal Thoppay',
+  author_email = 'write2sudarson@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['model', 'machinelearning'], # arbitrary keywords
+  classifiers = [],
+)
+
