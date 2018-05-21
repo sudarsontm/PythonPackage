ac7f2730a5aa5dabe4bcdb62e0311a9b468c2c08
T M Sudarson <write2sudarson.com>
2018-05-21 21:22:38 +0530

    check

---
+++ python_pkg/env/lib/python3.6/site-packages/simpstrlib/strlib.py
@@ -0,0 +1,8 @@
+def revStr(st):
+	return st[::-1]
+
+def addstr(st1,st2):
+	return st1 + ":" + st2 
+
+def addStars(st):
+	return "***" + st + "***"
---
+++ python_pkg/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'MOVA',
+  packages = ['MOVA'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simple String library',
+  author = 'Sudarson',
+  author_email = 'sudarsontm@outlook.com',
+  url = 'https://github.com/github.com/sudarsontm/Sudarson/tree/master/python_pkg/MOVA',
+  download_url = 'https://github.com/github.com/sudarsontm/Sudarson/tree/master/python_pkg/MOVA:/tarball/0.1',
+  keywords = ['string', 'reverse'], 
+  classifiers = [],
+)
